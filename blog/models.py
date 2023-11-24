0ea63f5d1055825f4c0401e9ab2adf0956968572
TANVIR RAHMAN <tanviredu2018@gmail.com>
2023-11-24 08:39:44 +0000

    added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db   import models
 from django.conf import settings
 
+class Comment(models.Model):
+    creator      = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content      = models.TextField()
 
 class Tag(models.Model):
     value        = models.TextField(max_length=100)
