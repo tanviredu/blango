5fd04e6bc0b94934bb04e1b8ef16acefb36a8007
tanviredu <tanviredu2018@gmail.com>
2023-11-24 14:56:11 +0600

    added

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,20 @@
 from django.db   import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
 
 class Comment(models.Model):
-    creator      = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    content      = models.TextField()
+    creator        = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content        = models.TextField()
+    
+    ##1) defines a foreign key with ContentType. that can be any model
+    ##2) we need an id as a foreign key for this content type
+    ##3) we made this a foreign key with content_type with the object_id
+    content_type   = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id      = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type','object_id')
 
 class Tag(models.Model):
     value        = models.TextField(max_length=100)
